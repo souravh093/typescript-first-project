4dbfb34028ed848bbd5c38f69ba1ab7504c353d4
Sourave halder <souravh093@gmail.com>
2024-05-29 20:02:54 +0600

    fix the string problem

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -8,7 +8,7 @@ import {
 const createAcademicSemesterValidationSchema = z.object({
   body: z.object({
     name: z.enum([...AcademicSemesterName] as [string, ...string[]]), // need help
-    year: z.date({ message: 'Year is Required' }),
+    year: z.string({ message: 'Year is Required' }),
     code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
     startMonth: z.enum([...Months] as [string, ...string[]]),
     endMonth: z.enum([...Months] as [string, ...string[]]),
